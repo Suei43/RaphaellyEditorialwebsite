b3d32c517adcc16bd6b5ed5f020ea65c7443c216
darksuei <chrisraphael576@gmail.com>
2024-04-19 14:45:15 +0100

    Feat: Major Refactor

--- blog/blog_1.js
+++ blog/blog_1.js
@@ -1,50 +1,57 @@
-const contact = document.querySelector('.contactus')
-const readMore = document.querySelector('.read-more')
-const Read = document.querySelector('.continue-read')
-const hamburger = document.querySelector("#hamburger")
-const menuIcons = document.querySelector(".menuicons")
-const navRespond = document.querySelector("#navrespond")
-const html = document.documentElement
-const line = document.querySelector('.line')
-const hideFirst = document.querySelectorAll(".hide-first")
+const body = document.querySelector("body");
+const hamburgerWrapper = document.querySelector(".hamburga");
+const hamburger = document.querySelector("#hamburger");
+const cancel = document.querySelector("#cancel");
+const contact = document.querySelector(".contactus");
+const readMore = document.querySelector(".read-more");
+const Read = document.querySelector(".continue-read");
+const menuIcons = document.querySelector(".menuicons");
+const navRespond = document.querySelector("#navrespond");
+const html = document.documentElement;
+const hideFirst = document.querySelectorAll(".hide-first");
 
-function openMenu(){
-    menuIcons.classList.add('animatee')
-    html.addEventListener('click', closeMenuOnBodyClick)
-}
-function closeMenu(){
-    menuIcons.classList.remove('animatee')
-    html.removeEventListener('click',closeMenuOnBodyClick)
-}
-function closeMenuOnBodyClick(event){
-    closeMenu();
+function closeNav(e) {
+  e.preventDefault();
+  menuIcons.classList.remove("activer");
+  navRespond.classList.remove("active");
+  hamburger.classList.remove("disabled");
+  cancel.classList.remove("active");
 }
 
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active")
-    menuIcons.classList.toggle("activer")
-    navRespond.classList.toggle("active")
-    line.classList.toggle("active")
-})
-document.querySelectorAll(".main-text").forEach(n =>n.addEventListener("click",
-() => {
-    hamburger.classList.remove("active")
-    navRespond.classList.remove("active")
-    menuIcons.classList.remove("activer")
-    line.classList.remove("active")
-}
-))
+body.addEventListener("click", closeNav);
 
-readMore.addEventListener("click", ()=>{
-    Read.style.display="block"
-    readMore.classList.add("button-clicked")
-    readMore.style.display="none"
-    hideFirst.forEach((member)=>{
-        member.classList.add("active")
-    })
-})
-if(window.innerWidth<=550){
-    hideFirst.forEach((member)=>{
-        member.classList.add("active")
-    })
-}   
+hamburgerWrapper.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.toggle("disabled");
+  cancel.classList.toggle("active");
+  menuIcons.classList.toggle("activer");
+  navRespond.classList.toggle("active");
+});
+
+navRespond.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+document.querySelectorAll(".main-text").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("disabled");
+    cancel.classList.remove("active");
+    navRespond.classList.remove("active");
+    menuIcons.classList.remove("activer");
+  })
+);
+
+readMore.addEventListener("click", () => {
+  Read.style.display = "block";
+  readMore.classList.add("button-clicked");
+  readMore.style.display = "none";
+  hideFirst.forEach((member) => {
+    member.classList.add("active");
+  });
+});
+
+if (window.innerWidth <= 550) {
+  hideFirst.forEach((member) => {
+    member.classList.add("active");
+  });
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const body = document.querySelector("body");
+const hamburgerWrapper = document.querySelector(".hamburga");
 const hamburger = document.querySelector("#hamburger");
+const cancel = document.querySelector("#cancel");
 const menuIcons = document.querySelector(".menuicons");
 const navRespond = document.querySelector("#navrespond");
 const resNav1 = document.querySelector("#resnavlinks1");
@@ -6,63 +9,72 @@ const resNav2 = document.querySelector("#resnavlinks2");
 const resNav3 = document.querySelector("#resnavlinks3");
 const resNav4 = document.querySelector("#resnavlinks4");
 const resNav5 = document.querySelector("#resnavlinks5");
-const html = document.documentElement;
 const mail = document.getElementById("instamail");
-const line = document.querySelector(".line");
 
-let menuOpen;
-function openMenu() {
-  menuIcons.classList.add("animatee");
-  html.addEventListener("click", closeMenuOnBodyClick);
+function closeNav(e) {
+  e.preventDefault();
+  menuIcons.classList.remove("activer");
+  navRespond.classList.remove("active");
+  hamburger.classList.remove("disabled");
+  cancel.classList.remove("active");
 }
-function closeMenu() {
-  menuIcons.classList.remove("animatee");
-  html.removeEventListener("click", closeMenuOnBodyClick);
-}
-function closeMenuOnBodyClick(event) {
-  closeMenu();
-}
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
+
+body.addEventListener("click", closeNav);
+
+hamburgerWrapper.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.toggle("disabled");
+  cancel.classList.toggle("active");
   menuIcons.classList.toggle("activer");
   navRespond.classList.toggle("active");
-  line.classList.toggle("active");
 });
+
 mail.addEventListener("click", () => {
   mail.style.backgroundColor = "#d61d1d";
 });
+
 document.querySelectorAll(".main-text").forEach((n) =>
   n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
+    hamburger.classList.remove("disabled");
+    cancel.classList.remove("active");
     navRespond.classList.remove("active");
     menuIcons.classList.remove("activer");
-    line.classList.remove("active");
   })
 );
-resNav1.addEventListener("click", () => {
+
+resNav1.addEventListener("click", (e) => {
+  e.stopPropagation();
   resNav1.classList.add("bg-active");
 });
-resNav2.addEventListener("click", () => {
-  hamburger.classList.remove("active");
+
+resNav2.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.remove("disabled");
+  cancel.classList.remove("active");
   navRespond.classList.remove("active");
   menuIcons.classList.remove("activer");
-  line.classList.remove("active");
   resNav2.classList.add("bg-active");
 });
-resNav3.addEventListener("click", () => {
-  hamburger.classList.remove("active");
+
+resNav3.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.remove("disabled");
+  cancel.classList.remove("active");
   navRespond.classList.remove("active");
   menuIcons.classList.remove("activer");
-  line.classList.remove("active");
   resNav3.classList.add("bg-active");
 });
-resNav4.addEventListener("click", () => {
+
+resNav4.addEventListener("click", (e) => {
+  e.stopPropagation();
   resNav4.classList.add("bg-active");
 });
-resNav5.addEventListener("click", () => {
-  hamburger.classList.remove("active");
+
+resNav5.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.remove("disabled");
+  cancel.classList.remove("active");
   navRespond.classList.remove("active");
   menuIcons.classList.remove("activer");
-  line.classList.remove("active");
   resNav5.classList.add("bg-active");
 });
